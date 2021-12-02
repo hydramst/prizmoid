d17b88919b63e1c88d78d6176f716192487294ad
Somertonman <8521878+Somertonman@users.noreply.github.com>
2021-12-02 19:07:16 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,10 +9,7 @@ import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
 
-print("TF Version: ", tf.__version__)
-#print("TF Hub version: ", hub.__version__)
-print("Eager mode enabled: ", tf.executing_eagerly())
-print("GPU available: ", tf.config.list_physical_devices('GPU'))
-
-
-st.write("d")
+st.write("TF Version: ", tf.__version__)
+st.write("TF Hub version: ", hub.__version__)
+st.write("Eager mode enabled: ", tf.executing_eagerly())
+st.write("GPU available: ", tf.config.list_physical_devices('GPU'))
