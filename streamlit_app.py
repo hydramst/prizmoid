7c4cf28486e9ae97e256c858c892d3998970c8a4
Somertonman <8521878+Somertonman@users.noreply.github.com>
2021-12-02 19:43:11 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,42 +19,6 @@ st.write("Eager mode enabled: ", tf.executing_eagerly())
 st.write("GPU available: ", tf.config.list_physical_devices('GPU'))
 
 
-def crop_center(image):
-  """Returns a cropped square image."""
-  shape = image.shape
-  new_shape = min(shape[1], shape[2])
-  offset_y = max(shape[1] - shape[2], 0) // 2
-  offset_x = max(shape[2] - shape[1], 0) // 2
-  image = tf.image.crop_to_bounding_box(
-      image, offset_y, offset_x, new_shape, new_shape)
-  return image
-
-@functools.lru_cache(maxsize=None)
-def load_image(image_url, image_size=(256, 256), preserve_aspect_ratio=True):
-  """Loads and preprocesses images."""
-  # Cache image file locally.
-  image_path = tf.keras.utils.get_file(os.path.basename(image_url)[-128:], image_url)
-  # Load and convert to float32 numpy array, add batch dimension, and normalize to range [0, 1].
-  img = tf.io.decode_image(
-      tf.io.read_file(image_path),
-      channels=3, dtype=tf.float32)[tf.newaxis, ...]
-  img = crop_center(img)
-  img = tf.image.resize(img, image_size, preserve_aspect_ratio=True)
-  return img
-
-def show_n(images, titles=('',)):
-  n = len(images)
-  image_sizes = [image.shape[1] for image in images]
-  w = (image_sizes[0] * 6) // 320
-  plt.figure(figsize=(w * n, w))
-  gs = gridspec.GridSpec(1, n, width_ratios=image_sizes)
-  for i in range(n):
-    plt.subplot(gs[i])
-    plt.imshow(images[i][0], aspect='equal')
-    plt.axis('off')
-    plt.title(titles[i] if len(titles) > i else '')
-  plt.show()
-  
   
   
 hub_handle = 'https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2'
