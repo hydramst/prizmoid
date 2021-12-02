04ce88881dbe2b590e0431d1ed638433134770e8
Somertonman <8521878+Somertonman@users.noreply.github.com>
2021-12-02 19:47:24 +0400

    mode added

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,6 @@ import time
 from matplotlib import gridspec
 import matplotlib.pylab as plt
 import numpy as np
-
 import tensorflow as tf
 import tensorflow_hub as hub
 os.environ['TFHUB_MODEL_LOAD_FORMAT'] = 'COMPRESSED'
@@ -24,7 +23,7 @@ st.write("GPU available: ", tf.config.list_physical_devices('GPU'))
 
   
   
-hub_handle = 'https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2'
+hub_handle = 'model/arbitrary-image-stylization-v1-256/2'
 hub_module = hub.load(hub_handle)
 
 content_image = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Golden_Gate_Bridge_from_Battery_Spencer.jpg/640px-Golden_Gate_Bridge_from_Battery_Spencer.jpg"
